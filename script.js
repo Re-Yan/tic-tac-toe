2e6a2d58abc391e89fc0cb6e6bfd1c28a3599221
Re-Yan <reannub@gmail.com>
2023-03-09 20:59:48 +0800

    Add DOM Elements and Click Events

---
+++ script.js
@@ -0,0 +1,50 @@
+"strict mode";
+const chosenSide = "X";
+
+// DOM elements
+const item1 = document.querySelector(".grid-item-1");
+const item2 = document.querySelector(".grid-item-2");
+const item3 = document.querySelector(".grid-item-3");
+const item4 = document.querySelector(".grid-item-4");
+const item5 = document.querySelector(".grid-item-5");
+const item6 = document.querySelector(".grid-item-6");
+const item7 = document.querySelector(".grid-item-7");
+const item8 = document.querySelector(".grid-item-8");
+const item9 = document.querySelector(".grid-item-9");
+
+// CLICK Events
+item1.addEventListener("click", () => {
+  item1.textContent = `${chosenSide}`;
+});
+
+item2.addEventListener("click", () => {
+  item2.textContent = `${chosenSide}`;
+});
+
+item3.addEventListener("click", () => {
+  item3.textContent = `${chosenSide}`;
+});
+
+item4.addEventListener("click", () => {
+  item4.textContent = `${chosenSide}`;
+});
+
+item5.addEventListener("click", () => {
+  item5.textContent = `${chosenSide}`;
+});
+
+item6.addEventListener("click", () => {
+  item6.textContent = `${chosenSide}`;
+});
+
+item7.addEventListener("click", () => {
+  item7.textContent = `${chosenSide}`;
+});
+
+item8.addEventListener("click", () => {
+  item8.textContent = `${chosenSide}`;
+});
+
+item9.addEventListener("click", () => {
+  item9.textContent = `${chosenSide}`;
+});
