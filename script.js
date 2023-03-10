fd06ec9c6f1a768e09906345f909b202a317c435
Re-Yan <reannub@gmail.com>
2023-03-11 03:06:43 +0800

    Implement Logic For Modal Buttons

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 "strict mode";
-const chosenSide = "X";
+let chosenSide;
 
 // DOM elements
 const item1 = document.querySelector(".grid-item-1");
@@ -11,6 +11,9 @@ const item6 = document.querySelector(".grid-item-6");
 const item7 = document.querySelector(".grid-item-7");
 const item8 = document.querySelector(".grid-item-8");
 const item9 = document.querySelector(".grid-item-9");
+const modal = document.querySelector(".modal");
+const buttonX = document.getElementById("buttonX");
+const buttonO = document.getElementById("buttonO");
 
 // CLICK Events
 item1.addEventListener("click", () => {
@@ -47,4 +50,14 @@ item8.addEventListener("click", () => {
 
 item9.addEventListener("click", () => {
   item9.textContent = `${chosenSide}`;
+});
+
+buttonX.addEventListener("click", () => {
+  chosenSide = "X";
+  modal.classList.add("hidden");
+});
+
+buttonO.addEventListener("click", () => {
+  chosenSide = "O";
+  modal.classList.add("hidden");
 });
